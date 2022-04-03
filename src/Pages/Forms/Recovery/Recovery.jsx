8692c118ac57dbb0798611387d5e02312e248a6d
DevCesar <74023876+devcesarhernandez@users.noreply.github.com>
2022-04-03 05:18:25 +0000

    Se agrego texto de ayuda en recuperar contraseña

--- src/Pages/Forms/Recovery/Recovery.jsx
+++ src/Pages/Forms/Recovery/Recovery.jsx
@@ -8,14 +8,16 @@ const Recobery = (props) => {
 	return (
 		<form>
 			<h1 className="text-center">Account Manager</h1>
-			<h2 className="fs-4 my-3">Recuperar contraseña</h2>
-			<div className="mb-3">
+			<h2 className="fs-4 mt-3">Recuperar contraseña</h2>
+			<small className="text-muted">Escribe la dirección de correo electrónico asociado a tu cuenta.</small>
+			<div className="my-3">
 				<Label>Correo electrónico</Label>
 				<Input/>
 			</div>
 			<Button className="btn btn-primary w-100 mb-2">Recuperar contraseña</Button>
 			<hr/>
-			<Link to="/login" className="btn btn-secondary w-100 mt-2">Iniciar sesión</Link>
+			<small className="text-muted text-center d-block">Regresar a</small>
+			<Link to="/login" className="btn btn-secondary w-100 mt-4">Iniciar sesión</Link>
 		</form>
 	)
 }
