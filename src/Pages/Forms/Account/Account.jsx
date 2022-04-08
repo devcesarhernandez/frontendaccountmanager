2ddd9c847c281665bae1d87f9e896801259e79b2
DevCesar <74023876+devcesarhernandez@users.noreply.github.com>
2022-04-08 17:06:26 +0000

    Tablas de Accounts, Platfoms y Categories asi com Routes de Account y Form

---
+++ src/Pages/Forms/Account/Account.jsx
@@ -0,0 +1,42 @@
+import React from "react"
+import Label from "../../../Components/Label/Label"
+import Input from "../../../Components/Inputs/Input"
+import Select from "../../../Components/Inputs/Select"
+import Button from "../../../Components/Button/Button"
+
+const options  = [
+	{
+		_id: "alfjasjfoiwej879",
+		name: "GMail",
+		description: "djsaj sdjlasasjj ldj ffjls flsfj asfaslñ ",
+	},
+	{
+		_id: "sjflkasf46asde",
+		name: "Mega",
+		description: "djsaj sdjlasasjj ldj ffjls flsfj asfaslñ ",
+	}
+]
+
+const Account = (props) => {
+	const { value, password} = props
+	return (
+		<form>
+			<h2>Registar nueva cuenta</h2>
+			<div className="mb-3">
+				<Label htmlFor="email">Correo electronico</Label>
+				<Input type="email" id="email"/>
+			</div>
+			<div className="mb-3">
+				<Label htmlFor="password">Contraseña</Label>
+				<Input type="password" id="email"/>
+			</div>
+			<div className="mb-3">
+				<Label htmlFor="platform">Plataforma:</Label>
+				<Select id="category" options={options} />
+			</div>
+			<Button type="submit" className="btn btn-success w-100">Registrar plataforma</Button>
+		</form>
+	)
+}
+
+export default Account
--- src/Routes/Platforms/Platform.jsx
+++ src/Routes/Platforms/Platform.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import { Routes, Route } from "react-router-dom"
 import Create from "../../Pages/Forms/Platform/Create"
+import Platforms from "../../Pages/Tables/Platforms/Platforms"
 
 const Platform = () => {
 	return (
 		<Routes>
 			<Route path="/platforms">
+				<Route index element={ <Platforms /> } />
 				<Route path="create" element={ <Create /> } />
 			</Route>
 		</Routes>
